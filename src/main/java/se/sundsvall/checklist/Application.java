56ad61f7fb7edc7387351e6f5332bcf344fe965d
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-11-26 08:20:49 +0100

    Fixed bean load issue

--- src/main/java/se/sundsvall/checklist/Application.java
+++ src/main/java/se/sundsvall/checklist/Application.java
@@ -5,17 +5,14 @@ import static org.springframework.boot.SpringApplication.run;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import se.sundsvall.dept44.ServiceApplication;
 
 @ServiceApplication
 @EnableFeignClients
 @EnableCaching
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "PT2M")
 public class Application {
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		run(Application.class, args);
 	}
 }
