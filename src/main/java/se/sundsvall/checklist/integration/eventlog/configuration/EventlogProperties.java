7384a46e20a4c8a1f619bd127d2331aa1494ee26
AndreSoderlund <andre.soderlund@b3.se>
2025-01-21 16:14:06 +0100

    Updated property classes for integrations

--- src/main/java/se/sundsvall/checklist/integration/eventlog/configuration/EventlogProperties.java
+++ src/main/java/se/sundsvall/checklist/integration/eventlog/configuration/EventlogProperties.java
@@ -1,10 +1,9 @@
 package se.sundsvall.checklist.integration.eventlog.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties("integration.eventlog")
 public record EventlogProperties(
-	@DefaultValue("5") int connectTimeout,
-	@DefaultValue("15") int readTimeout) {
+	int connectTimeout,
+	int readTimeout) {
 }
