376e497c0da04353e80efbdf73a0d0fcb7de9906
Joakim Schürer <joakim.schurer@sundsvall.se>
2025-04-01 14:10:13 +0200

    UF-15240: Added transactional to execute method
    - Added transactional to execute method
    - Added log row when email is being sent

--- src/main/java/se/sundsvall/checklist/service/CommunicationService.java
+++ src/main/java/se/sundsvall/checklist/service/CommunicationService.java
@@ -2,6 +2,8 @@ package se.sundsvall.checklist.service;
 
 import static org.zalando.problem.Status.NOT_FOUND;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.zalando.problem.Problem;
@@ -12,6 +14,7 @@ import se.sundsvall.checklist.service.mapper.CorrespondenceMapper;
 
 @Service
 public class CommunicationService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CommunicationService.class);
 
 	private final MailHandler mailHandler;
 
@@ -40,10 +43,11 @@ public class CommunicationService {
 		final var entity = employeeChecklistRepository.findByIdAndChecklistsMunicipalityId(employeeChecklistId, municipalityId)
 			.orElseThrow(() -> Problem.valueOf(NOT_FOUND, "Employee checklist with id %s not found within municipality %s.".formatted(employeeChecklistId, municipalityId)));
 
-		mailHandler.sendEmail(entity, emailTemplate);
+		sendEmail(entity);
 	}
 
 	public void sendEmail(EmployeeChecklistEntity entity) {
+		LOGGER.info("Sending email to {}, manager for new employee with login name {}", entity.getEmployee().getManager().getEmail(), entity.getEmployee().getUsername());
 		mailHandler.sendEmail(entity, emailTemplate);
 	}
 }
