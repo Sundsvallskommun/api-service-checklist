4d2cfe3e4acf362da0a53a4aee989a3e71b49444
cheezi0747 <linus.sjolinder@b3.se>
2025-10-07 08:40:48 +0200

    dependabot/maven/main/se.sundsvall.dept44-dept44-service-parent-6.0.13: update api spec

--- src/integration-test/java/openapi/OpenApiSpecificationIT.java
+++ src/integration-test/java/openapi/OpenApiSpecificationIT.java
@@ -1,9 +1,14 @@
 package openapi;
 
+import static java.nio.file.Files.writeString;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
-
+import net.javacrumbs.jsonunit.core.Option;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,11 +18,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
-
-import net.javacrumbs.jsonunit.core.Option;
 import se.sundsvall.checklist.Application;
 import se.sundsvall.dept44.util.ResourceUtils;
 
@@ -43,10 +43,12 @@ class OpenApiSpecificationIT {
 	private TestRestTemplate restTemplate;
 
 	@Test
-	void compareOpenApiSpecifications() {
+	void compareOpenApiSpecifications() throws IOException {
 		final var existingOpenApiSpecification = ResourceUtils.asString(openApiResource);
 		final var currentOpenApiSpecification = getCurrentOpenApiSpecification();
 
+		writeString(Path.of("target/generated-api.yaml"), currentOpenApiSpecification);
+		
 		assertThatJson(toJson(currentOpenApiSpecification))
 			.withOptions(List.of(Option.IGNORING_ARRAY_ORDER))
 			.whenIgnoringPaths("servers")
